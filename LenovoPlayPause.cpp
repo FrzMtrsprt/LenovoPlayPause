889a5690773c629d0542484c949803e2489bd0fc
FrzMtrsprt <alex20020703@163.com>
2022-08-16 22:53:36 +0800

    Switch to Visual Studio

--- LenovoPlayPause.cpp
+++ LenovoPlayPause.cpp
@@ -1,10 +1,14 @@
 #include "windows.h"
-int main()
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd)
 {
-    INPUT input;
-    input.type = INPUT_KEYBOARD;
-    input.ki.dwFlags = 0;
-    input.ki.wVk = VK_MEDIA_PLAY_PAUSE;
-    SendInput(1, &input, sizeof(INPUT));
-    return 0;
+	INPUT input{};
+	input.type = INPUT_KEYBOARD;
+	input.ki.dwFlags = 0;
+	input.ki.wVk = VK_MEDIA_PLAY_PAUSE;
+	SendInput(1, &input, sizeof(INPUT));
+	return 0;
 }
